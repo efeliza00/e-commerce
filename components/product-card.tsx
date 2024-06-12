88ad4a42328c04bdedecd903afdd2ce3b7abb9a0
dumvan <felizavndrd567@gmail.com>
2024-06-12 14:14:34 +0800

    chore(product-card): add rating

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { Rating } from 'react-simple-star-rating'
 import { Button } from './ui/button';
 
 type Product = {
@@ -30,10 +31,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, isAddtoCartButton = 
                 </div>
             </CardHeader>
             <CardContent className='h-2/6'>
-                <CardTitle className='h-2/3'>{product.title}</CardTitle>
+                <CardTitle className='max-h-full line-clamp-2'>{product.title}</CardTitle>
                 <h4 className="text-muted-foreground scroll-m-20 text-lg font-mediumtracking-tight">
                     $ {product?.price}
                 </h4>
+                <Rating size={20} readonly initialValue={Number(product.rating?.rate)} allowFraction tooltipDefaultText={product.rating?.rate} />
             </CardContent>
             {isAddtoCartButton === "show" && (
                 <CardFooter>
